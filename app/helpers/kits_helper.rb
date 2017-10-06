2fefbb6a9492470cf4b90c278879ecd5c6d0dcc4
IsNology <glenn@isnology.com>
2017-10-06 21:51:58 +1100

    add table_for helper

--- app/helpers/kits_helper.rb
+++ app/helpers/kits_helper.rb
@@ -9,30 +9,38 @@ module KitsHelper
     '$'+str.insert(-3, '.').join
   end
   
-  def table_header(obj, klass = nil)
+  # pass css class in option hash e.g.  class: 'table-striped'
+  # pass helper to a field via options hash using field name (symbol) as key and helper name (string) as the value
+  # e.g. table_for(@objs, field_name: 'dollors')
+  def table_for(objs, options = {})
     str = '<table'
-    klass ? str << ' class="'+klass+'">'+"\r\n" : str << ">\r\n"
-    str << "<thead>\r\n<tr>\r\n"
-    obj[0].attribute_names.each do |key|
-      unless key == 'id' or key == 'created_at' or key == 'updated_at'
-        name = key.to_s.gsub('_', ' ')
-        str << '<th>'+name+"</th>\r\n"
+    if (val = options[:class])
+       str << " class=\"#{val}\">"
+    else
+      str << '>'
+    end
+    str << '<thead><tr>'
+    objs[0].attribute_names.each do |f_name|
+      unless f_name == 'id' or f_name == 'created_at' or f_name == 'updated_at'
+        name = f_name.to_s.gsub('_', ' ')
+        str << '<th>'+name+'</th>'
       end
     end
-    str << "</tr>\n</thead>\n<tbody>"
-  end
-  
-  def table_row(obj)
-    str = "<tr>\r\n"
-    obj.attributes.each do |key, value|
-      unless key == 'id' or key == 'created_at' or key == 'updated_at'
-        str << '<td>'+value.to_s+"</td>\r\n"
+    str << '</tr></thead><tbody>'
+    objs.each do |obj|
+      str << '<tr>'
+      obj.attributes.each do |key, value|
+        unless key == 'id' or key == 'created_at' or key == 'updated_at'
+          if (val = options[key.to_sym])
+            str << "<td>#{__send__(val, value)}</td>"
+          else
+            str << "<td>#{value.to_s}</td>"
+          end
+        end
       end
+      str << '</tr>'
     end
-    str << "</tr>\r\n"
-  end
-  
-  def table_end
-    "</tbody>\r\n</table>\r\n"
+    str << '</tbody></table>'
+    str.html_safe
   end
 end
